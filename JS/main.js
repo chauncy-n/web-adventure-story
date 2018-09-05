496c269796de807388a35493b2073c345761ee53
chauncy-n <eldoon@comcast.net>
2018-09-05 09:57:46 -0700

    beginning of webpage design

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+button.onclick = function(){
+  document.getElementById("button").addEventListener('click', replaceText);
+
+  var replaceText = document.getElementById("demo").innerHTML = "try this"
+      
+      };      
+
+
